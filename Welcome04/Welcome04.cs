84a4914e3bb232e4c3b04af77028b6a52b15b7e4
papadage <gpapadakis@aegis-net.com>
2017-05-24 18:52:55 -0400

    Add project files.

---
+++ Welcome04/Welcome04.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Welcome04
+{
+   class Welcome04
+   {
+      static void Main()
+      {
+         string person = "George";
+         Console.WriteLine($"Welcome to C# Programming, {person}!");
+      }
+   }
+}
