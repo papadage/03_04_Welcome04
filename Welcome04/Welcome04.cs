a50331289f396ac0c1c97dd98604103e05b85fc5
papadage <gpapadakis@aegis-net.com>
2017-06-16 20:33:47 -0400

    Practice typing commands after lack of studying.

--- Welcome04/Welcome04.cs
+++ Welcome04/Welcome04.cs
@@ -1,13 +1,42 @@
-﻿using System;
+﻿// Figure 3.13: Welcome04.cs
+// Inserting content into a  string with string interpolation
 
-namespace Welcome04
+using System;
+
+class Welcome04 // Begin class Welcome04
 {
-   class Welcome04
+   static void Main() // Begin Main methoc
    {
-      static void Main()
-      {
-         string person = "George";
-         Console.WriteLine($"Welcome to C# Programming, {person}!");
-      }
-   }
-}
+      string person = "Paul"; // Declare string variable person, and initialize with vlaue "Paul"
+      Console.WriteLine($"Welcome to C# Programming, {person}!");
+
+   } // End Main method
+
+} //End class Welcome04
+
+
+
+//using System;
+
+//class Welcome04
+//{
+//   static void Main()
+//   {
+//      string person = "George";
+//      Console.WriteLine($"Welcome to C# Programming {person}");
+//   }
+//}
+
+//using System;
+
+//namespace Welcome04
+//{
+//   class Welcome04
+//   {
+//      static void Main()
+//      {
+//         string person = "George";
+//         Console.WriteLine($"Welcome to C# Programming, {person}!");
+//      }
+//   }
+//}
